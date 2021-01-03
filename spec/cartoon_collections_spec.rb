49f8db2b8fff2048e81a6b831f0d5ceb31138d43
Kwaku Gyapong <kwakugyapong219@gmail.com>
2021-01-03 06:10:16 +0000

    Done.

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -1,35 +1,21 @@
-require "spec_helper"
+def greet_characters(array)
+  
+  # array = ["snoopy", "toby", "scooby"]
+array.each do |character|
+    print "Hello #{character}! "
+end 
 
-describe "Using Each" do
-  describe "greet_characters" do
-    it "prints out a custom greeting for each name in the provided array" do
-      dwarves = ["Dopey", "Grumpy", "Bashful"]
+  # Use `each` to enumerate over the provided array
+  #
+  # Print a custom greeting for each element
+end
 
-      output = capture_stdout do
-        greet_characters(dwarves)
-      end
-
-      # This regex allows for arbitrary characters between
-      # the numbering and the name
-      expect(output).to match(/Hello Dopey!/)
-      expect(output).to match(/Hello Grumpy!/)
-      expect(output).to match(/Hello Bashful!/)
-    end
-  end
-
-  describe "list_dwarves" do
-    it "prints out the 7 dwarves in a numbered list" do
-      dwarves = ["Dopey", "Grumpy", "Bashful"]
-
-      output = capture_stdout do
-        list_dwarves(dwarves)
-      end
-
-      # This regex allows for arbitrary characters between
-      # the numbering and the name
-      expect(output).to match(/1.*Dopey/)
-      expect(output).to match(/2.*Grumpy/)
-      expect(output).to match(/3.*Bashful/)
-    end
-  end
+def list_dwarves(array)
+  array.each_with_index do |character, i |
+    i = i + 1 
+    print "#{i}.#{character} "
+  end 
+  # Use `each_with_index` to enumerate over the provided array
+  #
+  # Print a numbered list of each element
 end
